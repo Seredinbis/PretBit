2ad8e59528b7ac34ea6237cfa78fa5798f82a4d2
Seredinbis <78419583+Seredinbis@users.noreply.github.com>
2023-07-03 23:19:04 +0300

    3.07.23 теперь все ошибки и трекинг в кнопке Ошибки

--- handlers/errors.py
+++ handlers/errors.py
@@ -1,8 +1,10 @@
 from aiogram.types import Message, ErrorEvent
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, exceptions
-from bot import bot
+from bot import user_track
+from aiogram.filters import Text
 
+import support_function
 
 router_error = Router()
 
@@ -10,6 +12,13 @@ router_error = Router()
 @router_error.errors()
 async def er(er1: ErrorEvent, state: FSMContext) -> None:
     user_data = await state.get_data()
-    await bot.send_message(chat_id=327169698,
-                           text=f'БЫЛА ОШИБКА\nuser_id = {user_data["user_id"]}:{user_data["user_second_name"]}\n'
-                                f'ошибка = {er1.exception}')
+    user_track.append(f'БЫЛА ОШИБКА\nuser_id = {user_data["user_id"]}:{user_data["user_second_name"]}\n'
+                      f' ошибка = {er1.exception}\n ошибка = {exceptions}')
+
+
+@router_error.message(Text('Ошибки'))
+async def get_lebedki(message: Message, state: FSMContext) -> None:
+    msg = await message.answer(text=''.join(user_track))
+    await support_function.delete_pre_message.del_pre_message(chat_id=msg.chat.id,
+                                                              message_id=msg.message_id,
+                                                              state=state)
--- support_function/user_tracking.py
+++ support_function/user_tracking.py
@@ -1,9 +1,9 @@
 import datetime
-
+from bot import user_track
 
 async def where_who(where, state) -> None:
     user = await state.get_data()
     if "user_second_name" in user:
-        print(f'{user["user_second_name"]} : {user["user_id"]} in {where} : {datetime.datetime.now()}')
+        user_track.append(f'{user["user_second_name"]} : {user["user_id"]} in {where} : {datetime.datetime.now()}\n')
     else:
-        print(f'НЕ АВТОРИЗОВАННЫЙ ПОЛЬЗОВАТЕЛЬ НАЖАЛ {where} : {datetime.datetime.now()}')
+        user_track.append(f'НЕ АВТОРИЗОВАННЫЙ ПОЛЬЗОВАТЕЛЬ НАЖАЛ {where} : {datetime.datetime.now()}\n')
