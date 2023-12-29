1d641262ed1b34f33d4e2864c109a7d3f7659bec
Anton Seredin <78419583+Seredinbis@users.noreply.github.com>
2023-12-29 20:47:32 +0300

    redact back and calendar

--- handlers/back_button.py
+++ handlers/back_button.py
@@ -1,55 +1,102 @@
-import support_function
-
 from aiogram.filters import Text
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from keyboards.reply_markup.time_table import time_table_kb
-from keyboards.reply_markup.statements import choose_jenre_kb, choose_what_need_kb
-from keyboards.reply_markup.main import main_kb
-from keyboards.reply_inline.choose_show import choose_opera_kb, choose_balet_kb
-from keyboards.reply_markup.user_setting import user_settings_kb
+from config_data.data import TimeTable, Genre, ChoosePassport, Settings
+from config_data.secret import logs_path
+from keyboards.reply_markup.reply_builder import MainReplyKeyboard, MultiKeyboard
+from keyboards.reply_inline.inline_builder import DiskInlineKeyboard
+from disk_api.yandex_ import Passport
 from aiogram import Router
+from loguru import logger
+
+"""loging"""
+logger.add(logs_path+'/back_button.log', format='{time} {level} {message}', level='INFO', mode='w')
 
 router_back = Router()
 
 
-# Тут отлавлием переход назад с обычной клавиатуры
-@router_back.message(Text('Назад'))
-async def back(message: Message, state: FSMContext) -> None:
-    await support_function.user_tracking.where_who(where=message.text,
-                                                   state=state)
-    user_data = await state.get_data()
-    if await support_function.login_test.log_test(message=message):
-        keyboard = user_data['whitch_kb_was']
-        if keyboard == 'сhoose_jenre_kb':
-            msg = await message.answer(text='Пожалуйста выберите жанр',
-                                       reply_markup=choose_jenre_kb)
-        elif keyboard == 'time_table_kb':
-            msg = await message.answer(text='Пожалуйста выберите интересующий вас пункт',
-                                       reply_markup=time_table_kb)
-            await state.update_data(whitch_kb_was='main_kb')
-        elif keyboard == 'calendar_kb':
-            msg = await message.answer(text='Открываю главное меню!',
-                                       reply_markup=main_kb)
-        elif keyboard == 'user_settings_kb':
-            msg = await message.answer(text='Открываю главное меню!',
-                                       reply_markup=user_settings_kb)
-            await state.update_data(whitch_kb_was='main_kb')
-        elif keyboard == 'main_kb':
-            msg = await message.answer(text='Открываю главное меню!',
-                                       reply_markup=main_kb)
-        elif keyboard == 'choose_opera_kb':
-            msg = await message.answer(text='Пожалуйста выберите спектакль!',
-                                       reply_markup=choose_opera_kb)
-        elif keyboard == 'choose_balet_kb':
-            msg = await message.answer(text='Пожалуйста выберите спектакль!!',
-                                       reply_markup=choose_balet_kb)
-        elif keyboard == 'сhoose_what_need_kb':
-            msg = await message.answer(text='Пожалуйста выберите вид выписки!',
-                                       reply_markup=choose_what_need_kb)
-        else:
-            msg = await message.answer(text='Проблема в choose_kb')
-        await message.delete()
-        await support_function.delete_pre_message.del_pre_message(chat_id=msg.chat.id,
-                                                                  message_id=msg.message_id,
-                                                                  state=state)
+# # Тут отлавлием переход назад с обычной клавиатуры
+# @router_back.message(Text('Назад'))
+# async def back(message: Message, state: FSMContext) -> None:
+#     user_data = await state.get_data()
+#     keyboard = user_data['whitch_kb_was']
+#     if keyboard == 'сhoose_jenre_kb':
+#         keyboard = MultiKeyboard(Genre).build()
+#         msg = await message.answer(text='Пожалуйста выберите жанр',
+#                                    reply_markup=keyboard.as_markup())
+#     elif keyboard == 'time_table_kb':
+#         keyboard = MultiKeyboard(TimeTable).build()
+#         msg = await message.answer(text='Пожалуйста выберите интересующий вас пункт',
+#                                    reply_markup=keyboard.as_markup())
+#         await state.update_data(whitch_kb_was='main_kb')
+#     elif keyboard == 'calendar_kb':
+#         keyboard = MainReplyKeyboard().build()
+#         msg = await message.answer(text='Открываю главное меню!',
+#                                    reply_markup=keyboard.as_markup())
+#     elif keyboard == 'user_settings_kb':
+#         keyboard = MultiKeyboard(Settings).build()
+#         msg = await message.answer(text='Открываю главное меню!',
+#                                    reply_markup=keyboard.as_markup())
+#         await state.update_data(whitch_kb_was='main_kb')
+#     elif keyboard == 'main_kb':
+#         keyboard = MainReplyKeyboard().build()
+#         msg = await message.answer(text='Открываю главное меню!',
+#                                    reply_markup=keyboard.as_markup())
+#     elif keyboard == 'choose_opera_kb':
+#         genre = Passport('Опера')
+#         keyboard = await DiskInlineKeyboard(genre).build()
+#         msg = await message.answer(text='Пожалуйста выберите спектакль!',
+#                                    reply_markup=keyboard.as_markup())
+#     elif keyboard == 'choose_balet_kb':
+#         genre = Passport('Балет')
+#         keyboard = await DiskInlineKeyboard(genre).build()
+#         msg = await message.answer(text='Пожалуйста выберите спектакль!!',
+#                                    reply_markup=keyboard.as_markup())
+#     elif keyboard == 'сhoose_what_need_kb':
+#         keyboard = MultiKeyboard(ChoosePassport).build()
+#         msg = await message.answer(text='Пожалуйста выберите вид выписки!',
+#                                    reply_markup=keyboard.as_markup())
+#     else:
+#         msg = await message.answer(text='Проблема в choose_kb')
+#     await message.delete()
+#     await support_function.delete_pre_message.del_pre_message(chat_id=msg.chat.id,
+#                                                               message_id=msg.message_id,
+#                                                               state=state)
+
+
+class HandlerBack:
+    """Обработчик апдейтов от BackRouter"""
+
+    def __init__(self, message: Message, state: FSMContext):
+        self.message = message
+        self.state = state
+
+    async def handler(self):
+        keyboard_mapping = {
+            'сhoose_jenre_kb': (MultiKeyboard(Genre), 'Пожалуйста выберите жанр'),
+            'time_table_kb': (MultiKeyboard(TimeTable), 'Пожалуйста выберите интересующий вас пункт'),
+            'calendar_kb': (MainReplyKeyboard(), 'Открываю главное меню!'),
+            'user_settings_kb': (MultiKeyboard(Settings), 'Открываю главное меню!'),
+            'main_kb': (MainReplyKeyboard(), 'Открываю главное меню!'),
+            'choose_opera_kb': (await DiskInlineKeyboard(Passport('Опера')).build(), 'Пожалуйста выберите спектакль!'),
+            'choose_balet_kb': (await DiskInlineKeyboard(Passport('Балет')).build(), 'Пожалуйста выберите спектакль!!'),
+            'сhoose_what_need_kb': (MultiKeyboard(ChoosePassport), 'Пожалуйста выберите вид выписки!')
+        }
+
+        user_data = await self.state.get_data()
+        keyboard, answer_text = keyboard_mapping.get(user_data['whitch_kb_was'], (None, 'Ошибка клавиатуры'))
+        if keyboard is None:
+            logger.error(f'Ошибка в клавиатуре, данные: keyboard:{keyboard}, answer:{answer_text}')
+            await self.message.answer(text=answer_text)
+        await self.state.update_data(whitch_kb_was='main_kb')
+        await self.message.answer(text=answer_text, reply_markup=keyboard.as_markup())
+        await self.message.delete()
+
+
+class BackRouter(HandlerBack):
+    """Роутер, который принимает апдейты"""
+
+    @staticmethod
+    @router_back.message(Text('Назад'))
+    async def back(message: Message, state: FSMContext):
+        super().__init__(message, state)
