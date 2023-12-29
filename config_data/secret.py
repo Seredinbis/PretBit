8af3759f2c87be901ee99033450cd2ac6e7b2c80
Anton Seredin <78419583+Seredinbis@users.noreply.github.com>
2023-12-29 17:01:14 +0300

    full replace yandex

--- config_data/secret.py
+++ config_data/secret.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 yandex_token: str = os.getenv('YANDEX_TOKEN')
 logs_path: str = os.getenv('LOGS_PATH')
+bot_token: str = os.getenv('BOT_TOKEN')
+google_token: dict = os.getenv('GOOGLE_SHEETS_TOKEN')
+dev_id: str = os.getenv('DEV_ID')
+sql_url: str = os.getenv('SQL_URL')
